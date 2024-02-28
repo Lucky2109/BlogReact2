2f671f1587c50ce503393b5eeb54f501d6264195
Lucky2109 <lukao2109@gmail.com>
2024-02-28 15:19:08 -0300

    adicionado export na const

--- src/components/navbar/Navbar2.tsx
+++
@@ -1,4 +0,0 @@
-import React, { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../contexts/AuthContext'
-import { toastAlerta } from '../../util/toastAlerta'
